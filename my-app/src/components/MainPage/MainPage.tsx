c11df6db9367ed08287e3fded9743efd5ae97fc0
Tomasz Baran <tomasz.baran@microsoft.wsei.edu.pl>
2021-03-21 14:48:24 +0100

    LAB 2 (almost done- problems with imgs)

---
+++ my-app/src/components/MainPage/MainPage.tsx
@@ -0,0 +1,29 @@
+import React,{FC} from 'react';
+import styled from 'styled-components';
+
+import {TopBar} from '../TopBar/TopBar';
+import {LeftMenu} from '../LeftMenu/LeftMenu';
+
+const Wrapper = styled.section`
+
+`;
+
+const Content = styled.div`
+    max-width: 1200px;
+    align-items: center;
+    display: flex;
+`;
+
+const MainPage: FC = () => {
+    return (
+        <Wrapper>
+            <TopBar/>
+            <Content>
+                <LeftMenu/>
+                <div>content</div>
+            </Content>
+        </Wrapper>
+    );
+};
+
+export default MainPage;
