32b4c67399d32c3c9f2055ab6ddd85673886e1e5
Tomasz Baran <tomasz.baran@microsoft.wsei.edu.pl>
2021-04-12 22:33:41 +0200

    Added ExpandedMenu

--- src/components/TopBar/ExpandedMenu.tsx
+++ src/components/TopBar/ExpandedMenu.tsx
@@ -1,7 +1,29 @@
 import { FC } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    position: absolute;
+    top:24px;
+    left: 0;
+    background:white;
+    padding:16px;
+    border: 1px solid;
+    width:250px;
+    ul{
+        border: 1px solid red;
+    }
+`;
 
 export const ExpandedMenu: FC = () => {
     return (
-        <>ExpandedMenu</>
+        <Wrapper>
+            <ul>
+                <li>First</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>Last</li>
+            </ul>
+        </Wrapper>
     );
 };
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,5 +1,6 @@
 import {FC} from 'react';
 import styled from 'styled-components';
+import useDropdown from 'react-dropdown-hook';
 
 import {Wrapper} from '../../styledHelpers/Components';
 import {Colors} from '../../styledHelpers/Colors';
@@ -7,6 +8,7 @@ import {ExpandedMenu} from './ExpandedMenu';
 
 const Wrapper2 = styled(Wrapper)`
     padding: 10px;
+    background-color:red;
 `;
 
 const InnerWrapper = styled.div`
@@ -15,12 +17,10 @@ const InnerWrapper = styled.div`
 `;
 
 const RightIcons = styled.div`
-
+    text-align: right;
 `;
 
-const InputWrapper = styled.img`
 
-`;
 
 const CustomImg = styled.img`
 
@@ -30,17 +30,48 @@ const CustomInput = styled.input`
 
 `;
 
-export const TopBar: FC = () => {
+const Menuwrapper = styled.div`
+    position:relative;
+    cursor:pointer;
+    align-items:center;
+    width:250px;
+    justify-content:space-between;
+`;
+
+const LeftSide = styled.div`
+
+`;
+
+export const TopBar: FC = ()  => {
+    const [wrapperRef, dropdownOpen, toggleDropdown] = useDropdown();
+
+    const menuHandler = () => {
+        toggleDropdown();
+    };
+
     return (
         <Wrapper2>
             <InnerWrapper>
-                <CustomImg src="./media/logo.png"/>
+                <CustomImg src="./media/logo.png" alt=""/>
+                <Menuwrapper ref={wrapperRef}>
+                    <LeftSide>
+                        <img src="./media/icons/house.png"/>
+                        <span>Home</span>
+                        <img src="./media/icons/arrow-down.png" alt="" onClick={menuHandler}/>
+                    </LeftSide>
+
+                    {dropdownOpen &&
+                        <ExpandedMenu/>
+                    }
+                    </Menuwrapper>
+
+
                 <div>
-                    <ExpandedMenu/>
+
                 </div>
+
                 <InnerWrapper>
                     <CustomInput type="text"/>
-                    <input type="text"/>
                     <CustomImg src="./media/icons/search.png" alt="" title=""/>
                 </InnerWrapper>
                 <RightIcons>
