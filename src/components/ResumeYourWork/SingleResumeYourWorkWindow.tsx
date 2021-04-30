66390cee6ec86768d612851727ef2368fb5f0c1c
PanWidmo <tomasz.baranww@gmail.com>
2021-04-30 20:53:22 +0200

    Few Resume your work changes

--- src/components/ResumeYourWork/SingleResumeYourWorkWindow.tsx
+++ src/components/ResumeYourWork/SingleResumeYourWorkWindow.tsx
@@ -8,12 +8,23 @@ const InnerWrapper = styled.div`
     background: ${Colors.white};
     margin-top:30px;
     padding-left:20px;
+    padding-right:20px;
     h1{
         font-size:${fontSize[20]};
+        color:${Colors.blue};
+        margin-bottom:10px;
+
     }
 
     p{
         font-size:${fontSize[18]};
+    }
+`;
+
+const Bottom = styled.div`
+    margin:10px 0px;
+    img{
+        width:20px;
     }
 `;
 
@@ -23,6 +34,11 @@ export const SingleResumeYourWorkWindow: FC = () => {
         <InnerWrapper>
             <h1>World company SAS</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et hendrit orci. Donec vehicula justo ut nulla aliquet, ac tincidunt metus tristique.</p>
+            <Bottom>
+                <img src="./media/logo.png"/>
+                Subsid. corp.
+
+            </Bottom>
         </InnerWrapper>
     );
 };
