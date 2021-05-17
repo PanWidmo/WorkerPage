fe896debaaec8568d1e25b449e60a5863dcad772
PanWidmo <tomasz.baranww@gmail.com>
2021-05-17 20:13:43 +0200

    Added needed interfaces to entities

--- src/reducers/usersReducers.ts
+++ src/reducers/usersReducers.ts
@@ -3,12 +3,10 @@ import * as actionTypes from '../actions/actionTypes/userTypes';
 
 export interface IUsersReducer{
     usersList: ISingleUser[];
-    someData:string;
 }
 
 const defaultState = (): IUsersReducer => ({
     usersList: [],
-    someData: 'Ala ma kota'
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -21,13 +19,13 @@ export default (state = defaultState(), action: any) => {
                 usersList: data.usersList
             }
         }
-        case actionTypes.PUSH_DATA: {
-            const data: actionTypes.IUserTypes['PUSH_DATA'] = action;
-            return {
-                ...state,
-                someData: data.someData
-            }
-        }
+        // case actionTypes.PUSH_DATA: {
+        //     const data: actionTypes.IUserTypes['PUSH_DATA'] = action;
+        //     return {
+        //         ...state,
+        //         someData: data.someData
+        //     }
+        // }
 
         default: {
             return state
